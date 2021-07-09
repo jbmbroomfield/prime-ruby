ec288deecce712db7570e391ff9f34fcf3f30064
jim <jbmbroomfield@gmail.com>
2021-07-09 22:00:52 +1000

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+    if number < 2
+        return false
+    end
+    divisor = 2
+    while divisor <= number**0.5
+        number % divisor == 0 ? (return false) : divisor += 1
+    end
+    true
+end
